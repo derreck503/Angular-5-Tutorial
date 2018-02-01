e00f5d87c85b26d537f4ec4c370d707e163eac6f
Derreck Stellpflug <derreck.stellpflug@slalom.com>
2018-02-01 15:35:57 -0600

    Update 121

--- PROJECT/src/app/recipes/recipe.service.ts
+++ PROJECT/src/app/recipes/recipe.service.ts
@@ -1,17 +1,42 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
+    constructor(private shoppingListService: ShoppingListService){}
+
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'http://travel.home.sndimg.com/content/dam/images/travel/fullset/2014/07/20/32/food-paradise-102-ss-001.rend.hgtvcom.966.544.suffix/1491584380240.jpeg'),
-        new Recipe('And Another Test Recipe', 'This is simply a test', 'http://cdn-image.foodandwine.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1502824044/royal-farms-best-gas-station-food-FT-SS0817.jpg?itok=jJwvdRJB')
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+    [
+        new Ingredient('Salmon', 1),
+        new Ingredient('Lemon Slices', 6),
+        new Ingredient('Spinach', 3)
+    ]),
+        new Recipe('Another Test Recipe', 'This is simply a test', 'http://travel.home.sndimg.com/content/dam/images/travel/fullset/2014/07/20/32/food-paradise-102-ss-001.rend.hgtvcom.966.544.suffix/1491584380240.jpeg', 
+        [
+            new Ingredient('Bread', 1),
+            new Ingredient('Salomy', 2),
+            new Ingredient('Lettuce', 4),
+            new Ingredient('Cheese', 4),
+        ]),
+        new Recipe('And Another Test Recipe', 'This is simply a test', 'http://cdn-image.foodandwine.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1502824044/royal-farms-best-gas-station-food-FT-SS0817.jpg?itok=jJwvdRJB', 
+        [
+            new Ingredient('Chicken Drumstick', 3),
+            new Ingredient('Chicken Finger', 3)
+        ])
     ];
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
--- PROJECT/src/app/shopping-list/shopping-list.service.ts
+++ PROJECT/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,31 @@
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
+
 export class ShoppingListService {
 
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients:Ingredient[]  = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ];
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        // for (let ingredient of ingredients) {
+        //     this.addIngredient(ingredient);
+        // }
+
+        ///... is the spread operator to pass all array objects at once, instead of the whole array itself
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
 }
