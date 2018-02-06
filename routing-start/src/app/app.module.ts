a4cd593a4005f4f53b65e636d60daad2924e578a
Derreck Stellpflug <derreck.stellpflug@slalom.com>
2018-02-06 08:04:48 -0600

    update 149

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,21 +12,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children:[
-    { path: 'users/:id/:name', component: UserComponent }
-  ] },
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent},
-    { path: ':id/edit', component: EditServerComponent}    
-  ]},
-  { path: 'not-found', component: PageNotFoundComponent},
-  { path: '**', redirectTo: '/not-found' }
-];
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver';
 
 @NgModule({
   declarations: [
@@ -37,15 +29,17 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
+    // RouterModule.forRoot(appRoutes)
   ],
-  providers: [ServersService],
+  providers: [ServersService, ServerResolver, AuthGuard, AuthService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Router } from '@angular/router';
+import { Data } from '@angular/router/src/config';
 
 @Component({
   selector: 'app-server',
@@ -14,11 +15,8 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.queryParams['id'];
-    this.server = this.serversService.getServer(id);
-    this.route.params.subscribe(
-    (param: Params) => {
-      this.server = this.serversService.getServer(+param['id']);
+    this.route.data.subscribe( (data: Data) => {
+      this.server = data['server'];
     });
   }
 
